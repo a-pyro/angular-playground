8e208b9b3e42a9e0d67ea41a6a622dc125af942b
a-pyro <ardi.germenji@gmail.com>
2022-12-20 17:01:05 -0500

    brings in tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { GameBoardComponent } from './components/game-board/game-board.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GameBoardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
