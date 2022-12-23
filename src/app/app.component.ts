1af19765d1c6e145041f5e99741245ac014352c5
a-pyro <ardi.germenji@gmail.com>
2022-12-23 12:31:21 -0500

    adds api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  a = 1;
+  words: string[] = [];
+
+  ngOnInit() {
+    fetch(`${environment.API_URL}&wordLength=5`, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': environment.X_RapidAPI_Key,
+        'X-RapidAPI-Host': environment.X_RapidAPI_Host,
+      },
+    })
+      .then((response) => response.json())
+      .then((r) => (this.words = [...r]));
+  }
+}
